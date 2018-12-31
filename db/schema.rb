d32a449c4069308bd7bd60be0cc9e038d351eb2b
1575454118@qq.com <ycl798996@hotmail.com>
2018-12-31 13:41:09 +0800

    学分统计

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-AC
+
 ActiveRecord::Schema.define(version: 20181224010726) do
 
   # These are extensions that must be enabled in order to support this database
